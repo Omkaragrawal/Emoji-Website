883764b7eca2ddd7f2310f13e734e83869dc6294
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2020-04-21 23:22:26 +0530

    top 95 emojis

---
+++ MergerEmoji/index.js
@@ -0,0 +1,70 @@
+const mergeImg = require('merge-img');
+const Jimp = require("jimp");
+const fs = require('fs');
+const util = require('util');
+
+// mergeImg(["./1f606.png", "./1f606.png", "./1f606.png", "./1f606.png", "./1f606.png", "./1f606.png"], {
+//     offset: 5,
+//     direction: false,
+//     color: 0xffffff01
+// }).then ( img1 => {
+//     mergeImg(["./1f606.png", "./1f606.png", "./1f606.png", "./1f606.png", "./1f606.png", "./1f606.png"], {
+//         offset: 5,
+//         direction: false,
+//         color: 0xffffff01
+//     }). then ( img2 => {
+//         mergeImg([img1, img2], {
+//             direction: true,
+//         color: 0xffffff01
+//         }).then(imgFinal => {
+//             imgFinal.write('unicode.jpg', () => console.log("Done"))
+//         }) .catch( err => console.log(err));
+//     }).catch( err => console.log(err));
+// }) .catch( err => console.log(err));
+
+
+// (async () => {
+//     try {
+//         const image = await Jimp.read('./1f606.png');
+
+//         await image.resize(125, 125).background(0xffffffff);
+
+//         await image.writeAsync('./1f606(trial).jpg');
+
+//         const mergedImg = await mergeImg(['./1f606(trial).jpg', './1f606(trial).jpg', './1f606(trial).jpg', './1f606(trial).jpg', './1f606(trial).jpg', './1f606(trial).jpg'], {
+//             offset: 5,
+//             direction: false,
+//             color: 0xffffff01
+//         });
+//         await mergedImg;
+//         const finalImg = await mergeImg([mergedImg, mergedImg, mergedImg, mergedImg, mergedImg], {
+//             direction: true,
+//             color:0xffffff01
+//         });
+
+//         await finalImg.write("./1f606(final).jpg", _ => console.log("DONE"));
+//         await finalImg.write("./1f606(final-1).jpg", _ => console.log("DONE"));
+//     } catch (err) {
+//         console.log(err)
+//     }
+
+// })()
+
+(async => {
+    const dir1 = './topEmojiDataset/ImageNew-Google/'
+    const dir2 = './topEmojiDataset/Messengericons1/'
+    let topList = JSON.parse(fs.readFileSync('./todownload.json'));
+    topList = topList["top200"];
+    console.log(topList.length)
+     topList = topList.filter(imgUnit => {
+        if(fs.existsSync(dir1 + imgUnit.id.toLowerCase() + ".png") && (fs.existsSync(dir2 + imgUnit.id.toLowerCase() + ".png"))) {
+            return true;
+        } else {
+            return false;
+        }
+    }).map(imgUnit => imgUnit.id.toLowerCase())
+
+    fs.writeFileSync('finalList.json', JSON.stringify({list: topList}));
+
+    console.log(topList.length);
+})()
